0b8a634871c6958b070afd45fc51aae03c042998
Phinny01 <phinehas.fuachie20@gmail.com>
2024-03-30 16:49:34 -0400

    Removed volatile from variables, cleaned up code

--- main.cpp
+++ main.cpp
@@ -7,19 +7,19 @@ auto int_benchmark() {
     auto start = high_resolution_clock::now();
 
     // 10^10 additions
-   volatile int sum = 0; // Use volatile to discourage optimization
+    int sum = 0; 
     for(long long i = 0; i < 10000000000LL; ++i) {
         sum += 1; // Simple addition
     }
 
-    // 5 x 10^9 multiplications, corrected to prevent overflow
-  volatile int mul = 1;
+    // 5 x 10^9 multiplications
+   int mul = 1;
     for(long long i = 0; i < 5000000000LL; ++i) {
         mul = mul * 2;
     }
 
-    // 2 x 10^9 divisions, corrected to do something trivial but not optimized away
-  volatile int div = 1;
+    // 2 x 10^9 divisions
+   int div = 1;
     for(long long i = 0; i < 2000000000LL; ++i) {
         div /= 2; // Simple division
     }
