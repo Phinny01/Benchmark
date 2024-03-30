b0f18068284e058fc1327baba3b210609d529ae1
Phinny01 <104809511+Phinny01@users.noreply.github.com>
2024-03-30 19:06:33 +0000

    added benchmark for for int multiplication

--- main.cpp
+++ main.cpp
@@ -7,4 +7,9 @@ int main() {
   for(long long i = 0; i < 10000000000LL; ++i) {
       sum += 1;// Simple addition
   }
+  // 5 x 10^9 multiplications
+  int  mul = 1;
+  for(long long i = 0; i < 5000000000LL; ++i) {
+      mul *= 2; // Simplified operation for benchmarking
+  }
 }
